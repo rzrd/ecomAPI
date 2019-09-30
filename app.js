b5d9ff9d0787e1efeaf32d339180f07e9d029258
Muhammad Rozi Ardiansyah <roziardiansyah@gmail.com>
2019-09-30 15:25:10 +0700

    cors

--- app.js
+++ app.js
@@ -4,12 +4,14 @@ var app = express()
 var port = process.env.PORT || 3000
 var bodyparser = require('body-parser')
 var mongoose = require('mongoose')
+var cors = require('cors')
 var config_server = process.env.DB_ATLAS || process.env.DB_LOCAL
 //var config_server = process.env.DB_LOCAL
 
 
 mongoose.connect(config_server, {useNewUrlParser: true, useUnifiedTopology: true})
 
+app.use(cors())
 app.use(bodyparser.json())
 
 require('./app/routes/user.route')(app)
